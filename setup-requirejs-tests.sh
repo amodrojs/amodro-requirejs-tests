c3d5595c1c5e9e2bb443f29b3868a2a7b032cc51
jrburke <jrburke@gmail.com>
2015-06-13 22:53:34 -0700

    init

---
+++ setup-requirejs-tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone --depth 1 git@github.com:requirejs/domReady.git
+git clone --depth 1 git@github.com:requirejs/text.git
+git clone --depth 1 git@github.com:requirejs/i18n.git
+mkdir tests
